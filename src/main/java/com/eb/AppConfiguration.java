7552f66d659981be73f0570507ab5d32d83f3935
Enes Bahtiyar <97892255+enesbahtiyar@users.noreply.github.com>
2023-07-14 13:39:31 +0300

    properties environment

--- src/main/java/com/eb/AppConfiguration.java
+++ src/main/java/com/eb/AppConfiguration.java
@@ -1,10 +1,13 @@
 package com.eb;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
+import java.util.Properties;
 import java.util.Random;
 
 @Configuration
@@ -16,9 +19,22 @@ public class AppConfiguration
         @Component:    Class level
         @Bean:         Bean level
      */
+
+    @Autowired
+    private Environment environment;
+
     @Bean
     public Random random()
     {
         return new Random();
+    }
+
+    @Bean
+    public Properties properties()
+    {
+        Properties properties = new Properties();
+        properties.put("email", environment.getProperty("app.email"));
+        properties.put("phone", environment.getProperty("app.phone"));
+        return properties;
     }
 }
